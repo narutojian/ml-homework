ac2a7434a3799cdb52aa19d8eb4665b0f84b7d47
narutojian <narutojian666@gmail.com>
2023-01-16 15:31:42 +0800

    modified:   gan.py

--- gan.py
+++ gan.py
@@ -181,4 +181,4 @@ for epoch in range(args.n_epochs):
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % args.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+            save_image(gen_imgs.data[:16], "images/%d.png" % batches_done, nrow=5, normalize=True)
